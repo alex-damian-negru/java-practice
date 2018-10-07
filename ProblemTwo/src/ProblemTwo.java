5d62111653ce21095fd28855ddea99e92c551a6f
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-07 22:26:23 +0300

    Separate values by comma in a byte array

--- ProblemTwo/src/ProblemTwo.java
+++ ProblemTwo/src/ProblemTwo.java
@@ -1,4 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ProblemTwo {
 
+	public static void main(String[] args) {
+		readFile();
+	}
+	
+	private static void readFile() {
+		File file = new File("resources/cipher.txt");
+		try (Scanner scanner = new Scanner(file).useDelimiter(",");){
+			while(scanner.hasNext()) {
+				System.out.print(scanner.next());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
