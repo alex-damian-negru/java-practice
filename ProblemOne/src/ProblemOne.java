b12c11f07242e6d5f4d5f6361b2f0f5a7488d052
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-07 12:14:36 +0300

    Present alternate implementation; choose final version later

--- ProblemOne/src/ProblemOne.java
+++ ProblemOne/src/ProblemOne.java
@@ -3,26 +3,18 @@ import java.util.Arrays;
 
 public class ProblemOne {
 
-	/*
-	 * Generate all prime numbers up to n using the Sieve of Erastothenes algorithm.
-	 */
-	private boolean[] initPrimes(int n) {
-		boolean primes[] = new boolean[n + 1];
+	// Generate all prime numbers up to n using the Sieve of Erastothenes algorithm.
+	private ArrayList<Integer> generatePrimes(int limit) {
+		ArrayList<Integer> primesList = new ArrayList<Integer>();
+		boolean primes[] = new boolean[limit + 1];
 		Arrays.fill(primes, true);
 
-		for (int p = 2; p * p <= n; p++) {
+		for (int p = 2; p * p <= limit; p++) {
 			if (primes[p] == true) {
-				for (int i = p * 2; i <= n; i += p)
+				for (int i = p * 2; i <= limit; i += p)
 					primes[i] = false;
 			}
 		}
-		return primes;
-	}
-
-	private ArrayList<Integer> generatePrimes(int n) {
-
-		ArrayList<Integer> primesList = new ArrayList<Integer>();
-		boolean[] primes = initPrimes(n);
 
 		for (int i = 2; i < primes.length; i++) {
 			if (primes[i] == true) {
@@ -34,32 +26,57 @@ public class ProblemOne {
 	}
 
 	private int consecutivePrimeSum(int limit) {
-		boolean[] isPrime = initPrimes(limit);
-		ArrayList<Integer> primes = generatePrimes(limit);
+		int result = 0;
+		int numberOfPrimes = 0;
+		Integer[] primes = generatePrimes(limit).toArray(new Integer[0]);
+		int[] primeSum = new int[primes.length + 1];
 
-		int maxSum = 0;
-		int maxRun = -1;
-		for (int i = 0; i < primes.size(); i++) {
-			int sum = 0;
-			for (int j = i; j < primes.size(); j++) {
-				sum += primes.get(j);
-				if (sum > limit)
+		primeSum[0] = 0;
+		for (int i = 0; i < primes.length; i++)
+			primeSum[i + 1] = primeSum[i] + primes[i];
+
+		for (int i = numberOfPrimes; i < primeSum.length; i++) {
+			for (int j = i - (numberOfPrimes + 1); j >= 0; j--) {
+				if (primeSum[i] - primeSum[j] > limit)
 					break;
-				else if (j - i > maxRun && sum > maxSum && isPrime[sum]) {
-					maxSum = sum;
-					maxRun = j - i;
+
+				if (Arrays.binarySearch(primes, primeSum[i] - primeSum[j]) >= 0) {
+					numberOfPrimes = i - j;
+					result = primeSum[i] - primeSum[j];
 				}
 			}
 		}
-		System.out.println(maxSum);
-		return maxSum;
+
+		System.out.println(result + " " + numberOfPrimes);
+		return result;
 	}
+	// private int consecutivePrimeSum(int limit) {
+	// boolean[] isPrime = initPrimes(limit);
+	// ArrayList<Integer> primes = generatePrimes(isPrime);
+	//
+	// int maxSum = 0;
+	// int maxRun = -1;
+	// for (int i = 0; i < primes.size(); i++) {
+	// int sum = 0;
+	// for (int j = i; j < primes.size(); j++) {
+	// sum += primes.get(j);
+	// if (sum > limit)
+	// break;
+	// else if (j - i > maxRun && sum > maxSum && isPrime[sum]) {
+	// maxSum = sum;
+	// maxRun = j - i;
+	// }
+	// }
+	// }
+	// System.out.println(maxSum);
+	// return maxSum;
+	// }
 
 	public static void main(String args[]) {
 		ProblemOne problemOne = new ProblemOne();
 
 		long start = System.currentTimeMillis();
-		problemOne.consecutivePrimeSum(1000);
+		problemOne.consecutivePrimeSum(1000000);
 		long end = System.currentTimeMillis();
 		System.out.println("Executed in " + (end - start) + "ms");
 	}
