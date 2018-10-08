da358ee40894ddb7a9e1dc968ebcc5104b0148a9
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-08 16:04:16 +0300

    Find key

--- ProblemTwo/src/ProblemTwo.java
+++ ProblemTwo/src/ProblemTwo.java
@@ -30,7 +30,7 @@ public class ProblemTwo {
 	private static List<Byte> readFile() {
 		List<Byte> bytes = new ArrayList<Byte>();
 		File file = new File("resources/cipher.txt");
-		
+
 		try (Scanner scanner = new Scanner(file).useDelimiter(",")) {
 			while (scanner.hasNext()) {
 				int value = Integer.parseInt(scanner.next());
@@ -39,7 +39,7 @@ public class ProblemTwo {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		return bytes;
 	}
 
@@ -59,30 +59,44 @@ public class ProblemTwo {
 	private static int frequencyAnalysis(String text) {
 		final String MOSTCOMMON = "etaoinshrdlu";
 		int score = 0;
-		
+
 		for (char letter : text.toLowerCase().toCharArray()) {
 			for (char common : MOSTCOMMON.toCharArray()) {
 				if (letter == common)
 					score++;
 			}
 		}
-		
+
 		return score;
+	}
+
+	private static int findKey(Map<Integer, String> scores) {
+		Map.Entry<Integer, String> firstEntry = scores.entrySet().iterator().next();
+		int largestKey = firstEntry.getKey();
+
+		for (Map.Entry<Integer, String> map : scores.entrySet()) {
+			int key = map.getKey();
+			if (key > largestKey)
+				largestKey = key;
+		}
+		return largestKey;
 	}
 
 	private static void decrypt() {
 		List<Byte> encryptedText = readFile();
 		List<String> keys = generateKeys();
 		Map<Integer, String> textScores = new HashMap<Integer, String>();
-		
+
 		// Do XOR between the key and the values
 		for (String key : keys) {
 			StringBuilder plainText = new StringBuilder(); // Create new plainText
 			char[] testKey = key.toCharArray(); // Separate key to individual chars
 			int i = 0; // index for chars
-			
-			for (Byte character : encryptedText) { // Create text by XOR between each char and its' corresponding key char
-				if (i == 3) i = 0;
+
+			for (Byte character : encryptedText) { // Create text by XOR between each char and its' corresponding key
+													// char
+				if (i == 3)
+					i = 0;
 				char currChar = (char) (character ^ testKey[i]);
 				plainText.append(currChar);
 				i++;
@@ -90,8 +104,7 @@ public class ProblemTwo {
 			int score = frequencyAnalysis(plainText.toString());
 			textScores.put(score, key);
 		}
-		
-		for (Map.Entry<Integer, String> entry : textScores.entrySet())
-		    System.out.println("Key: " + entry.getKey() + " / " + entry.getValue());
+		System.out.println(findKey(textScores));
+
 	}
 }
