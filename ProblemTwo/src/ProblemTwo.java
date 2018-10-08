1dac1aba1a7e912d80a0cb39e2f7a808b9b997de
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-08 13:20:52 +0300

    Generate all key combinations and repeat them for every character

--- ProblemTwo/src/ProblemTwo.java
+++ ProblemTwo/src/ProblemTwo.java
@@ -7,20 +7,61 @@ import java.util.Scanner;
 public class ProblemTwo {
 
 	public static void main(String[] args) {
-		readFile();
+		decrypt();
 	}
-	
-	private static void readFile() {
+
+	private static List<Byte> readFile() {
 		List<Byte> bytes = new ArrayList<Byte>();
 		File file = new File("resources/cipher.txt");
-		
-		try (Scanner scanner = new Scanner(file).useDelimiter(",");){
-			while(scanner.hasNext()) {
+
+		try (Scanner scanner = new Scanner(file).useDelimiter(",")) {
+			while (scanner.hasNext()) {
 				int value = Integer.parseInt(scanner.next());
-				bytes.add((byte)value);
+				bytes.add((byte) value);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		}
+		
+		return bytes;
+	}
+
+	private static List<String> generateKeys() {
+		List<String> keyList = new ArrayList<String>();
+
+		for (char a = 'a'; a <= 'z'; a++) {
+			for (char b = 'a'; b <= 'z'; b++) {
+				for (char c = 'a'; c <= 'z'; c++) {
+					keyList.add("" + a + b + c);
+				}
+			}
+		}
+		return keyList;
+	}
+
+	private static void findKey() {
+		
+	}
+	
+	private static void decrypt() {
+		// Get encrypted file & all key combinations
+		List<Byte> encryptedText = readFile();
+		List<String> keys = generateKeys();
+		
+		// Put every key underneath the text
+		for (String key : keys) {
+			for (int i = 0; i < encryptedText.size(); i++) {
+				System.out.print(encryptedText.get(i));
+			}
+			System.out.println();
+			
+			char[] testKey = key.toCharArray();
+			for (int i = 0, j = 0; i < encryptedText.size(); i++) {
+				if (j == 3) j = 0;
+				System.out.print(testKey[j]);
+				j++;
+			}
+			System.out.println("\n");
 		}
 	}
 }
